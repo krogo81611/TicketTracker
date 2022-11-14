6ea82c8a0c2a114e2ab5ee4f29b565e4a8af022d
krogo81611 <krogo81611@gmail.com>
2022-11-14 10:17:55 -0700

    Commit chnages

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+
 const fetchIssues = () => {
-    let issues = JSON.parse(localStorage.getItem('issues'))
-    let issuesList = document.getElementById('issuesList')
+    let issues = JSON.parse(localStorage.getItem('issues'));
+    let issuesList = document.getElementById('issuesList');
 
     issuesList.innerHTML = '';
 
     for (let i = 0; i<issues.length;i++) {
-        let id = issues[i].id
-        let subject = issues[i].subject
-        let description = issues[i].description
-        let severity = issues[i].severity
-        let assignedTo = issues[i].assignedTo
-        let status = issues[i].status
+        let id = issues[i].id;
+        let subject = issues[i].subject;
+        let description = issues[i].description;
+        let severity = issues[i].severity;
+        let assignedTo = issues[i].assignedTo;
+        let status = issues[i].status;
 
-        let statusColor = status == 'Closed' ? 'label-success' : 'label-info'
+        let statusColor = status == 'Closed' ? 'label-success' : 'label-info';
 
         issuesList.innerHTML += 
 
+    }
+}
+
+const saveIssue = e => {
+    let issueId = chance.guid();
+    let issueSubject = document.getElementById('issueSubjInput').value;
+    let issueDescription = document.getElementById('issueDescInput').value;
+    let issueSeverity = document.getElementById('issueSeverityInput').value;
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    let issueStatus = 'Open'
+
+    let issue = {
+        id : issueId,
+        subject : issueSubject,
+        description : issueDescription,
+        severity : issueSeverity,
+        assignedTo : issueAssignedTo,
+        status : issueStatus
     }
 }
